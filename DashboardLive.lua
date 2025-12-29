ab8842d1c558d24a75430e96d29341c11c372c10
Glowins Modschmiede <martin@eller-essen.de>
2025-12-29 15:38:00 +0100

    2.D.7.6

--- DashboardLive.lua
+++ DashboardLive.lua
@@ -3243,7 +3243,7 @@ function DashboardLive.getDashboardLiveBase(self, dashboard)
 				returnValue = g_gameSettings:getValue("radioVolume")
 			elseif o == "station" then
 				local senderName = g_soundPlayer.currentChannelName
-				local len = string.len(dashboard.textMask or "xxxxxxxx")
+				local len = string.len(dashboard.textMask or "xxxx")
 				local alignment = dashboard.textAlignment or "LEFT"
 				returnValue = trim(senderName, len, alignment)
 			else
