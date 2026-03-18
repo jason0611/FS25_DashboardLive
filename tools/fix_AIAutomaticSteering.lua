d53b353780541ce21297f4a8239769e069a85119
Glowins Modschmiede <martin@eller-essen.de>
2026-03-19 00:30:34 +0100

    Aisteering bugfix (#87)
    * step 1
    
    * Update DashboardLive.lua
    
    * Schritt 2
    
    * 2.1.0.0-BETA-21
    
    * Update fix_AIAutomaticSteering.lua
    
    * Update DashboardLive.lua

---
+++ tools/fix_AIAutomaticSteering.lua
@@ -0,0 +1,11 @@
+-- game fixes
+--function AIAutomaticSteering:onWriteUpdateStream(streamId, connection, dirtyMask)
+
+function fixAIAutomaticSteering_onWriteUpdateStream(self, superfunc, streamId, connection, dirtyMask)
+	if self.spec_aiAutomaticSteering.steeringFieldCourse == nil then
+		streamWriteBool(streamId, false)
+	else
+		superfunc(self, streamId, connection, dirtyMask)
+	end
+end
+AIAutomaticSteering.onWriteUpdateStream = Utils.overwrittenFunction(AIAutomaticSteering.onWriteUpdateStream, fixAIAutomaticSteering_onWriteUpdateStream)
