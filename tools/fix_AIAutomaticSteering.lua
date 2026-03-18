2938c9ce90a863c56056ecf75303b2a1b185c0b2
Glowins Modschmiede <martin@eller-essen.de>
2026-03-19 00:26:33 +0100

    Update fix_AIAutomaticSteering.lua

--- tools/fix_AIAutomaticSteering.lua
+++ tools/fix_AIAutomaticSteering.lua
@@ -7,14 +7,5 @@ function fixAIAutomaticSteering_onWriteUpdateStream(self, superfunc, streamId, c
 	else
 		superfunc(self, streamId, connection, dirtyMask)
 	end
-
---	local spec = self.spec_aiAutomaticSteering
---	if not connection:getIsServer() then
---		if streamWriteBool(streamId, spec.steeringFieldCourse ~= nil and (bit32.band(dirtyMask, self.spec_aiAutomaticSteering.dirtyFlag) ~= 0)) then
---			if spec.steeringFieldCourse ~= nil then
---				spec.steeringFieldCourse:writeSegmentStatesToStream(streamId, connection)
---			end
---		end
---	end
 end
 AIAutomaticSteering.onWriteUpdateStream = Utils.overwrittenFunction(AIAutomaticSteering.onWriteUpdateStream, fixAIAutomaticSteering_onWriteUpdateStream)
